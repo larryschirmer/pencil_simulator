7c81c1b7dc4c8d4b7c52fcb9ee3c8e3803189d40
larryschirmer <larryschirmer@gmail.com>
2017-05-10 12:29:50 -0400

    write coverage tests for wrappers used in Pencils .from() method

--- test/Wrapper_Test.js
+++ test/Wrapper_Test.js
@@ -3,7 +3,19 @@
 let { assert, expect } = require('chai');
 const nativeAssert = require('assert');
 
-let { roundDown, hasNegitive, hasString, throwError, getCost, degrade } = require('../wrapper');
+let {
+      roundDown,
+      hasNegitive,
+      hasString,
+      throwError,
+      getCost,
+      degrade,
+      writeWord,
+      convertToWordArray,
+      convertToLetterArray,
+      eraseLetters,
+      restoreString
+} = require('../wrapper');
 
 describe('Wrapper Coverage Tests', function() {
       //beforeEach(function() {});
@@ -124,6 +136,101 @@ describe('Wrapper Coverage Tests', function() {
             it('returns 0 if amount is over degraded', function() {
                   let cost = 7;
                   assert.deepEqual(degrade(cost, strength), 0);
+            });
+      });
+
+      describe('writeWord()', function() {
+            let string;
+            let health;
+            beforeEach(function() {
+                  string = 'hello';
+                  health = 2;
+            });
+
+            it('exists', function() {
+                  assert.isOk(writeWord);
+            });
+
+            it('returns substring of original string', function() {
+                  assert.deepEqual(writeWord(string, health), 'he');
+            });
+      });
+
+      describe('convertToWordArray()', function() {
+            let string;
+            beforeEach(function() {
+                  string = 'hello world';
+            });
+
+            it('exists', function() {
+                  assert.isOk(convertToWordArray);
+            });
+
+            it('returns an array of sepperate words', function() {
+                  assert.deepEqual(convertToWordArray(string), ['hello', 'world']);
+            });
+      });
+
+      describe('convertToLetterArray()', function() {
+            let string;
+            beforeEach(function() {
+                  string = 'hello';
+            });
+
+            it('exists', function() {
+                  assert.isOk(convertToLetterArray);
+            });
+
+            it('returns an array of sepperate words', function() {
+                  assert.deepEqual(convertToLetterArray(string), ['h', 'e', 'l', 'l', 'o']);
+            });
+      });
+
+      describe('eraseLetters()', function() {
+            let arr, remove, wordNumber;
+            beforeEach(function() {
+                  arr = ['hello'];
+                  wordNumber = 0;
+            });
+
+            it('exists', function() {
+                  assert.isOk(eraseLetters);
+            });
+
+            it('should remove partial words in reverse letter order', function() {
+                  remove = 2;
+                  assert.deepEqual(eraseLetters(remove, wordNumber, arr), [
+                        ['h', 'e', 'l', ' ', ' ']
+                  ]);
+            });
+
+            it('should remove whole words', function() {
+                  remove = 5;
+                  assert.deepEqual(eraseLetters(remove, wordNumber, arr), [
+                        [' ', ' ', ' ', ' ', ' ']
+                  ]);
+            });
+
+            it('should stop removing letters when word length is reached', function() {
+                  remove = 15;
+                  assert.deepEqual(eraseLetters(remove, wordNumber, arr), [
+                        [' ', ' ', ' ', ' ', ' ']
+                  ]);
+            });
+      });
+
+      describe('restoreString()', function() {
+            let string;
+            beforeEach(function() {
+                  string = [['h', 'e', 'l', 'l', 'o']];
+            });
+
+            it('exists', function() {
+                  assert.isOk(restoreString);
+            });
+
+            it('returns a string from an array', function() {
+                  assert.deepEqual(restoreString(string), 'hello');
             });
       });
 });
